ab0470e007bd7d6b1de7aa4c0b412f96d02dcbe4
unknown <robertvuia06@gmail.com>
2023-11-26 12:44:31 +0200

    Nice workflow on client

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -1,5 +1,32 @@
 #include "client.h"
 
 Client::Client(uint64_t(*scrambleFunc)(uint64_t))
-    : kq::client_interface<messageType>(scrambleFunc)
-{}
+    : kq::client_interface<messageType>(scrambleFunc),
+	logger("client_output.txt", "./"),
+	networkingThread(&Client::networkInitAndLoop, this), networkLoop(true)
+{
+
+}
+
+Client::~Client()
+{
+	logger.out(KQINFO, { "Disconnecting from server...\n" });
+	networkLoop = false;
+	Disconnect();
+}
+
+
+void Client::networkInitAndLoop()
+{
+	logger.out(KQINFO, { "Connecting to server...\n" });
+	if (Connect("192.168.0.105", 60000))
+	{
+		logger.out(KQINFO, { "Failed connecting to server!\n" });
+		return;
+	}
+	logger.out(KQINFO, { "Successfuly connected to server!\n" });
+	while (networkLoop)
+	{
+		
+	}
+}
