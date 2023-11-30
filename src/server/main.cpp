80e50e174dea91c0ed8d7d4e5515b3ecd5e6ac2a
Robert <34575005+Robertkq@users.noreply.github.com>
2023-11-30 02:55:47 +0200

    Created keyboardManager & better CMake build system (#7)

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -5,18 +5,8 @@
 int main()
 {
 	Server master{ 60000, &validation_function };
-	std::cout << "Starting server...\n";
-	if (!master.Start())
-	{
-		std::cerr << "Server failed start up!\n";
-		return 1;
-	}
-	std::cout << "Server started successfuly!\n";
-	while (true)
-	{
-		master.Update();
-	}
-	std::cout << "Stopping server...\n";
-	master.Stop();
+	master.StartServer(); // blocking
+	master.StopServer(); // currently unreachable
+	
 	return 0;
 }
